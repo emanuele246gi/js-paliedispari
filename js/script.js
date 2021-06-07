82e07666590db12e84b3531a5d11ac813b9e7d84
Emanuele <emanuele65b@gmail.com>
2021-06-07 17:55:53 +0200

    add somma

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
+// Primo dado
+var primo = prompt('Scegli un numero tra 1 e 5');
+
+// Secondo dado
+var secondo = Math.floor(Math.random() * 5 + 1);
+
+// Risultato
+sum = primo + secondo;
+
 // Numeri pari o dispari
-
-
 function evenodd(x){
     if (x % 2 == 0){
-        return "pari"
+        return "pari";
     }
 
     else {
-        return "dispari"
-        console.log(Hai )
+        return "dispari";
     }
 }
+
+var risultato;
+
+if (primo > secondo){
+    risultato = 'Vittoria!';
+}
+
+else if (primo < secondo){
+    risultato = 'Ritenta!';
+}
+
+else{
+    risultato = 'Pareggio!';
+}
+
+document.getElementById('primo').innerHTML += primo;
+document.getElementById('secondo').innerHTML += secondo;
+document.getElementById('risultato').innerHTML = risultato;
